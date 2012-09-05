dfff4c82d1cf2e680b003dd57e6db53881871cf1
Arttu Multanen <karethoth@gmail.com>
2012-09-05 21:08:59 +0300

    CEGUI works now, really.

--- src/viqo/TestScene.cpp
+++ src/viqo/TestScene.cpp
@@ -1,6 +1,26 @@
 #include "TestScene.hpp"
 
 
+CEGUI::MouseButton ConvertButton( OIS::MouseButtonID buttonID )
+{
+  switch( buttonID )
+  {
+    case OIS::MB_Left:
+      return CEGUI::LeftButton;
+
+    case OIS::MB_Right:
+      return CEGUI::RightButton;
+
+    case OIS::MB_Middle:
+      return CEGUI::MiddleButton;
+
+    default:
+      return CEGUI::LeftButton;
+  }
+}
+
+
+
 TestScene::TestScene()
 {
 }
@@ -22,6 +42,10 @@ void TestScene::CreateScene()
   CEGUI::Scheme::setDefaultResourceGroup("Schemes");
   CEGUI::WidgetLookManager::setDefaultResourceGroup("LookNFeel");
   CEGUI::WindowManager::setDefaultResourceGroup("Layouts");
+
+  CEGUI::SchemeManager::getSingleton().create("TaharezLook.scheme");
+  CEGUI::System::getSingleton().setDefaultMouseCursor("TaharezLook", "MouseArrow");
+
 
   mSceneMgr->setAmbientLight( Ogre::ColourValue( 0,0,0 ) );
   mSceneMgr->setShadowTechnique( Ogre::SHADOWTYPE_TEXTURE_MODULATIVE );
@@ -53,5 +77,101 @@ void TestScene::CreateScene()
 
 void TestScene::DestroyScene()
 {
+}
+
+
+
+void TestScene::CreateFrameListener()
+{
+  Ogre::LogManager::getSingletonPtr()->logMessage("*** Initializing OIS ***");
+  OIS::ParamList pl;
+  size_t windowHnd = 0;
+  std::ostringstream windowHndStr;
+
+  mWindow->getCustomAttribute("WINDOW", &windowHnd);
+  windowHndStr << windowHnd;
+  pl.insert(std::make_pair(std::string("WINDOW"), windowHndStr.str()));
+
+  mInputManager = OIS::InputManager::createInputSystem( pl );
+
+  mKeyboard = static_cast<OIS::Keyboard*>(mInputManager->createInputObject( OIS::OISKeyboard, true ));
+  mMouse = static_cast<OIS::Mouse*>(mInputManager->createInputObject( OIS::OISMouse, true ));
+
+  mMouse->setEventCallback(this);
+  mKeyboard->setEventCallback(this);
+
+  //Set initial mouse clipping size
+  windowResized(mWindow);
+
+  //Register as a Window listener
+  Ogre::WindowEventUtilities::addWindowEventListener(mWindow, this);
+
+  mRoot->addFrameListener(this);
+}
+
+
+
+bool TestScene::frameRenderingQueued( const Ogre::FrameEvent &evt )
+{
+  if( mWindow->isClosed() )
+    return false;
+
+  if( mShutDown )
+    return false;
+
+  mKeyboard->capture();
+  mMouse->capture();
+
+  CEGUI::System::getSingleton().injectTimePulse( evt.timeSinceLastFrame );
+
+  return true;
+}
+
+
+
+
+bool TestScene::keyPressed( const OIS::KeyEvent &arg )
+{
+  CEGUI::System &sys = CEGUI::System::getSingleton();
+  sys.injectKeyDown(arg.key);
+  sys.injectChar(arg.text);
+  return true;
+}
+
+
+
+bool TestScene::keyReleased( const OIS::KeyEvent &arg )
+{
+  CEGUI::System::getSingleton().injectKeyUp(arg.key);
+  return true;
+}
+
+
+
+bool TestScene::mousePressed( const OIS::MouseEvent &arg, OIS::MouseButtonID id )
+{
+  CEGUI::System::getSingleton().injectMouseButtonDown( ConvertButton( id ) );
+  return true;
+}
+
+
+
+bool TestScene::mouseReleased( const OIS::MouseEvent &arg, OIS::MouseButtonID id )
+{
+  CEGUI::System::getSingleton().injectMouseButtonUp( ConvertButton( id ) );
+  return true;
+}
+
+
+
+bool TestScene::mouseMoved( const OIS::MouseEvent &arg )
+{
+  CEGUI::System &sys = CEGUI::System::getSingleton();
+  sys.injectMouseMove( arg.state.X.rel, arg.state.Y.rel );
+
+  if( arg.state.Z.rel )
+    sys.injectMouseWheelChange(arg.state.Z.rel / 120.0f);
+
+  return true;
 }
 
--- src/viqo/TestScene.hpp
+++ src/viqo/TestScene.hpp
@@ -9,8 +9,18 @@ class TestScene : public ViqoScene
  protected:
   virtual void CreateScene();
   virtual void DestroyScene();
+  virtual void CreateFrameListener();
+  virtual bool frameRenderingQueued( const Ogre::FrameEvent &evt );
   //virtual void CreateCamera();
   //virtual void CreateViewports();
+
+  virtual bool keyPressed( const OIS::KeyEvent &arg );
+  virtual bool keyReleased( const OIS::KeyEvent &arg );
+
+  virtual bool mouseMoved( const OIS::MouseEvent &arg );
+  virtual bool mousePressed( const OIS::MouseEvent &arg, OIS::MouseButtonID id );
+  virtual bool mouseReleased( const OIS::MouseEvent &arg, OIS::MouseButtonID id );
+
 
  public:
   TestScene();
